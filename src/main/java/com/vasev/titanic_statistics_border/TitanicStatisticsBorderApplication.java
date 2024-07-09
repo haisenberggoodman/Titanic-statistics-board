c4642108154fcfdc5d5d9012920722a1ebaa357c
haisenberggoodman <web.vasev@gmail.com>
2024-07-09 16:28:44 +0400

    Initial commit

---
+++ src/main/java/com/vasev/titanic_statistics_border/TitanicStatisticsBorderApplication.java
@@ -0,0 +1,13 @@
+package com.vasev.titanic_statistics_border;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitanicStatisticsBorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitanicStatisticsBorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vasev/titanic_statistics_border/TitanicStatisticsBorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasev.titanic_statistics_border;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitanicStatisticsBorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
